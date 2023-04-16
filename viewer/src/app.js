a7385312d3b52f4e56f20eb2365f0042b47b1fcb
Chi W. Ng <chi.w.ng@pisys.io>
2023-04-16 15:00:17 -0700

    update url link for ifc document

--- viewer/src/app.js
+++ viewer/src/app.js
@@ -77,14 +77,4 @@ const ifcLoader = new IFCLoader();
 // for local
 ifcLoader.ifcManager.setWasmPath("wasm/");
 const url = threeCanvas.getAttribute('location');
-// const input = document.getElementById("file-input");
-// input.addEventListener(
-//   "change",
-//   (changed) => {
-//     const file = changed.target.files[0];
-//     var ifcURL = URL.createObjectURL(file);
-//     ifcLoader.load(ifcURL, (ifcModel) => scene.add(ifcModel));
-//   },
-//   false
-// );
 ifcLoader.load(url, (ifcModel) => scene.add(ifcModel));
